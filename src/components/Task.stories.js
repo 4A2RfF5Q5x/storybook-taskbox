e41c7c5b44a277ece01ca98b3185bb9da5393701
VBasko <vvbasko@outlook.com>
2024-08-06 20:20:45 -0300

    feat: create Task story

---
+++ src/components/Task.stories.js
@@ -0,0 +1,48 @@
+import { fn } from "@storybook/test";
+import Task from "./Task.vue";
+
+export const ActionsData = {
+  onPickTask: fn(),
+  onArchiveTask: fn(),
+};
+
+export default {
+  component: Task,
+  title: 'Task',
+  tags: ['autodocs'],
+  excludeStories: /.*Data$/,
+  args: {
+    ...ActionsData
+  }
+};
+
+export const Default = {
+  args: {
+    task: {
+      id: '1',
+      state: 'TASK_INBOX',
+      title: 'Test Task',
+    },
+  },
+};
+
+export const Pinned = {
+  args: {
+    task: {
+      ...Default.args.task,
+      state: 'TASK_PINNED',
+    },
+  },
+};
+
+export const Archived = {
+  args: {
+    task: {
+      ...Default.args.task,
+      state: 'TASK_ARCHIVED',
+    },
+  },
+};
+
+
+
