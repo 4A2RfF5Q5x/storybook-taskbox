c192711e47baa149c74fdcdf690bcb2797c7b2b1
VBasko <vvbasko@outlook.com>
2024-08-10 13:44:06 -0300

    feat: create InboxScreen component with store logic

---
+++ src/components/InboxScreen/InboxScreen.vue
@@ -0,0 +1,15 @@
+<template>
+  <PureInboxScreen :error="isError" />
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+import { useTaskStore } from '@/store';
+
+import PureInboxScreen from '@/components/PureInboxScreen/PureInboxScreen.vue';
+
+const store = useTaskStore();
+
+const isError = computed(() => store.status==='error');
+</script>
