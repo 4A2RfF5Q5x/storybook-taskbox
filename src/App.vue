d6ca3a5f4acca8603870a82f382380a880ec8d8f
VBasko <vvbasko@outlook.com>
2024-08-10 13:44:30 -0300

    feat: add InboxScreen component to the App

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,13 @@
 <template>
-  <div>Hello world!</div>
+  <div id="app">
+    <InboxScreen />
+  </div>
 </template>
+
+<script setup>
+import InboxScreen from '@/components/InboxScreen/InboxScreen.vue';
+</script>
+
+<style>
+@import './index.css';
+</style>
