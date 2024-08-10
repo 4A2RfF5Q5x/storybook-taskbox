8c238505d90622914b525bb4df05077396fcc8d2
VBasko <vvbasko@outlook.com>
2024-08-10 13:43:25 -0300

    feat: mock pinia for storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,11 @@
-import '../src/index.css'
+import { setup } from '@storybook/vue3';
+import { createPinia } from 'pinia';
+
+import '../src/index.css';
+
+setup((app) => {
+  app.use(createPinia());
+});
 
 /** @type { import('@storybook/vue3').Preview } */
 const preview = {
@@ -6,10 +13,10 @@ const preview = {
     controls: {
       matchers: {
         color: /(background|color)$/i,
-        date: /Date$/
-      }
-    }
-  }
-}
+        date: /Date$/,
+      },
+    },
+  },
+};
 
-export default preview
+export default preview;
